b43713837cd4142048bcc164c0f87795ed702969
Andrea Tegria <andreategria3@gmail.com>
2022-11-26 18:16:22 -0500

    26-11-2022

--- src/components/usuarios/index.jsx
+++ src/components/usuarios/index.jsx
@@ -1,47 +1,59 @@
 import Table from "../table/Table";
-import { getData }  from "../../js/getData";
+import { useState } from "react";
+import { getData, getRequest } from "../../js/getData";
 import MainPage from "../table/MainPage";
 import CreateButton from "../table/CreateBotton";
 import SearchBar from "../table/SearchBar";
-
-
+import BodyIndex from "./bodyIndex";
+import { useEffect } from "react";
+import backendConfig from "../../js/backendConfig";
 
 let objCss = {
     border: "6px solid red",
 };
 
 function UsuariosIndex(props) {
-    let datosTabla = getData("url", {}, "get", {});
+    const [datosTabla, setDatosTabla] = useState([]);
+    const [state, setState] = useState("loading");
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        console.log(backendConfig.FULL_API_PATH + "usuarios/all");
+        let promiseData = getRequest(
+            backendConfig.FULL_API_PATH + "usuarios/all",
+            {},
+            "get",
+            {}
+        );
+
+        promiseData
+            .then(function (response) {
+                console.log(response);
+                setState("loaded");
+                setDatosTabla(response.data);
+            })
+            .catch(function (err) {
+                setState("error");
+                setError(err);
+                console.log(err);
+            });
+    }, []);
+    if (state === "error") {
+        return (
+            <div className="mx-3 d-flex">
+                <h3>{error.toString()}</h3>
+            </div>
+        );
+    }
+    if (state === "loading") {
+        return (
+            <div className="mx-3 d-flex">
+                <h3>Loading...</h3>
+            </div>
+        );
+    }
+    return <BodyIndex datosTabla={datosTabla}></BodyIndex>;
     //procesar datos
-    return (
-        <div>
-            <h3>Usuarios</h3>
-           
-          
-            <MainPage
-                data={datosTabla}
-                path={"/usuarios"}
-                name={"Tabla de Usuarios"}
-                columns={["id", "firstName", "email", "username", "password"]}
-                columnsAlias={[
-                    "ID",
-                    "Nombre",
-                    "Correo",
-                    "Usuario",
-                    "Contrasena",
-                ]}
-                tools={["update", "delete"]}
-            />
-            {/* <div className="d-flex">
-                <div className="col-6">
-                    <Table />
-                </div>
-                <div className="col-6">
-                    <Table />
-                </div>
-            </div> */}
-        </div>
-    );
 }
 
 export default UsuariosIndex;
